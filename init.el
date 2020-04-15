84db82ab3126c281d5be5b78b18ce0d600526156
Warlock <b.jurczok@gmail.com>
2020-04-15 18:06:41 +0200

    general changes in linking

--- init.el
+++ init.el
@@ -1,1 +1,45 @@
-/home/apprentice/.emacs.d/init.el
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(unless (package-installed-p 'solarized-theme)
+  (package-refresh-contents)
+  (package-install 'solarized-theme))
+
+(setq solarized-use-variable-pitch nil)
+(setq solarized-scale-org-headlines nil)
+(setq solarized-height-minus-1 1)
+(setq solarized-height-plus-1 1)
+(setq solarized-height-plus-2 1)
+(setq solarized-height-plus-3 1)
+(setq solarized-height-plus-4 1)
+(load-theme 'solarized-dark t)
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("5dbdb4a71a0e834318ae868143bb4329be492dd04bdf8b398fb103ba1b8c681a" default)))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (calfw-org calfw which-key web-mode use-package sudo-edit spaceline solarized-theme smex smart-hungry-delete slime-company rust-mode rainbow-delimiters projectile org-bullets nord-theme julia-mode ido-vertical-mode hungry-delete flycheck-clang-analyzer dashboard company-shell company-jedi company-irony company-c-headers avy))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
